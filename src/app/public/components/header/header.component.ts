872d7ccc5cdc11718bf3754fb021088fafe00bf8
Marco Collantes Artola <u201410183@upc.edu.pe>
2024-04-28 14:13:59 -0500

    fix: toolbar update

--- src/app/public/components/header/header.component.ts
+++ src/app/public/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from "@angular/material/toolbar";
-import { MatButton } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbar,MatButton],
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
