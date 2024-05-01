9f131ebe1a4dd974c22c7df64efd916dee63c885
Marco Collantes Artola <u201410183@upc.edu.pe>
2024-04-30 19:24:32 -0500

    feat: footer added

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../public/components/header/header.component";
+import { FooterComponent } from "../../../public/components/footer/footer.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MatCardModule, MatButtonModule],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
