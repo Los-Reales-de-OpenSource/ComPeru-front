81eae5f07cb0d52f5507dde4453fbb3c8efc3371
Marco Collantes Artola <72058326+Markollantes2307@users.noreply.github.com>
2024-05-04 11:18:01 -0500

    fix: for future merging

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../public/components/header/header.component";
 import { FooterComponent } from "../../../public/components/footer/footer.component";
+import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from '@angular/material/card';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
